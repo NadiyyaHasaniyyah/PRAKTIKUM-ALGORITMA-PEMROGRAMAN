1c8e32ecfa2f05c7380376f155e37c632771101e
Nadiyya <89526057+NadiyyaHasaniyyah@users.noreply.github.com>
2022-04-21 20:08:20 +0700

    Update SeqSearch.cpp

--- SeqSearch.cpp
+++ SeqSearch.cpp
@@ -1,7 +1,48 @@
-#include <iostream>
+/*Nama File 	: searching.cpp*/
+/*Deskripsi 	: searching*/
+/*Pembuat   	: 24060121120030 Nadiyya Hasaniyyah*/
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdio.h> /*header file*/
 
-int main(int argc, char** argv) {
-	return 0;
+void seqSearch(int Arr[], int N,int *iX, int X);
+
+/*Program Utama*/
+int main()
+{
+    /*Kamus*/
+    int Arr[] = {2,4,6,8,9,10,11,19,10,18};
+    int N = sizeof(Arr)/sizeof(Arr[0]);
+    int X = 18;
+    int iX;
+    
+    /*Algoritma*/
+    seqSearch(Arr,N,&iX, X);
+    if(iX== -1){
+        printf("Elemen tidak ditemukan");
+    }
+    else{
+        printf("Elemen ditemukan pada index ke %d\n", iX);
+    }
+
 }
+
+void seqSearch(int Arr[], int N, int *iX, int X)
+{
+    /*Kamus Lokal*/
+    int i; /*Counter*/
+
+    /*Algoritma*/
+    i = 1;
+    while(i < N && Arr[i-1] != X){ 
+        printf("%d\n", i);
+        i++;
+    }
+    if(Arr[i-1] == X){
+        *iX=i;
+    }
+    else{
+        *iX= -1;
+    }
+}
+
+
